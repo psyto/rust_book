52cf813be5595d92be0adbfd437c5e151c501e97
Hiroyuki Saito <saito.hiroyuki@gmail.com>
2022-03-04 09:40:14 +0900

    release build

---
+++ ch1-penguins/target/release/ch1-penguins.d
@@ -0,0 +1,1 @@
+/Users/psyto/rust_book/ch1-penguins/target/release/ch1-penguins: /Users/psyto/rust_book/ch1-penguins/src/main.rs
---
+++ ch1-penguins/target/release/deps/ch1_penguins-42c0e1369162a292.d
@@ -0,0 +1,5 @@
+/Users/psyto/rust_book/ch1-penguins/target/release/deps/ch1_penguins-42c0e1369162a292: src/main.rs
+
+/Users/psyto/rust_book/ch1-penguins/target/release/deps/ch1_penguins-42c0e1369162a292.d: src/main.rs
+
+src/main.rs:
